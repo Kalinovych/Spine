58b29f66b27e841673ae16fcc2fa201163aa50bb
Kalinovych <2morrowMan@gmail.com>
2021-08-04 00:23:02 +0300

    cleanup

--- Assets/Scripts/App/Models/MenuModel.cs
+++ Assets/Scripts/App/Models/MenuModel.cs
@@ -1,13 +1,8 @@
 ﻿using Spine;
-using UnityEngine;
 
 namespace App {
 	public class MenuModel : Model {
 		int screenIndex;
-
-		public MenuModel() {
-			Debug.LogWarning( $"MENU MODEL" );
-		}
 
 		public int ScreenIndex {
 			get => screenIndex;
