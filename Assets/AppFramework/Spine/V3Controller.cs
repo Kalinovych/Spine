67adbf934c6acf5264a4a12f42e7db6a46991ccb
flash <2morrowMan@gmail.com>
2021-09-02 12:27:28 +0300

    BuildContext()

--- Assets/AppFramework/Spine/V3Controller.cs
+++ Assets/AppFramework/Spine/V3Controller.cs
@@ -10,20 +10,22 @@ namespace Spine.Experiments {
 		void Start() {
 			print( "V3Mediator.Start" );
 
-			context = new Context()
-					.InstallEventHub()
-					.InstallCommandHub()
-					.ConfigureModel( configurator => {
-						configurator.Add<ICalculationService, AddCalculationService>();
-					} )
-					.ConfigureCommands( hub => {
-						hub.Map<CalculateRequest, CalculateCommand>();
-						hub.Map<CalculationResult, PrintResultToLog>();
-					} )
+			context = BuildContext()
 					.Send( new CalculateRequest( "2+2" ) )
 					.Send( new CalculateRequest( "2+3" ) )
 				;
 		}
+
+		static Context BuildContext() => new Context()
+			.InstallEventHub()
+			.InstallCommandHub()
+			.ConfigureModel( configurator => {
+				configurator.Add<ICalculationService, AddCalculationService>();
+			} )
+			.ConfigureCommands( hub => {
+				hub.Map<CalculateRequest, CalculateCommand>();
+				hub.Map<CalculationResult, PrintResultToLog>();
+			} );
 	}
 
 	readonly struct PrintResultToLog : ICommand<CalculationResult> {
@@ -61,17 +63,10 @@ namespace Spine.Experiments {
 	}
 
 	interface ICalculationService {
-		float Calc(string input);
-
 		void Calculate(string input);
 	}
 
 	class TwoPlusTwoCalculationService : ICalculationService {
-		public float Calc(string input) {
-			Debug.Log( $"TwoPlusTwoCalculationService.Calc: {input}" );
-			return 4;
-		}
-
 		[Inject] EventHub eventHub;
 
 		public void Calculate(string input) {
@@ -80,16 +75,16 @@ namespace Spine.Experiments {
 	}
 
 	class AddCalculationService : ICalculationService {
-		public float Calc(string input) {
-			Debug.Log( $"AddCalculationService.Calc: {input}" );
-			var parts = input.Split( '+' ).Select( float.Parse ).ToArray();
-			return parts[0] + parts[1];
-		}
-
 		[Inject] EventHub eventHub;
 
 		public void Calculate(string input) {
 			eventHub.Send( new CalculationResult( Calc( input ) ) );
+		}
+
+		public float Calc(string input) {
+			Debug.Log( $"AddCalculationService.Calc: {input}" );
+			var parts = input.Split( '+' ).Select( float.Parse ).ToArray();
+			return parts[0] + parts[1];
 		}
 	}
 }
